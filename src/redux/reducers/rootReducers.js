fa66a1d7291b46cd6d0f4f4d86628a8149105967
dimput <dimasputra@mail.ugm.ac.id>
2020-06-17 03:56:58 +0700

    push last night

--- src/redux/reducers/rootReducers.js
+++ src/redux/reducers/rootReducers.js
@@ -1,6 +1,7 @@
 import personReducers from './personReducers';
 import ticketReducers from './ticketReducers';
 import notificationReducer from './notificationReducer';
+import summaryReducers from './summaryReducer';
 import kbReducers from './kbReducers';
 import { combineReducers } from 'redux';
 
@@ -8,6 +9,7 @@ import { combineReducers } from 'redux';
 const rootReducers = combineReducers({
     personState: personReducers,
     kb: kbReducers,
+    summary:summaryReducers,
     notification:notificationReducer,
     ticketState: ticketReducers,
     databaru: "hello"
