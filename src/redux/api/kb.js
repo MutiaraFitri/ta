bd2e65656bd1abf9f8b5f7afaa55862a8b412258
dimput <dimasputra@mail.ugm.ac.id>
2020-07-20 01:08:43 +0700

    fixed bug kb all

--- src/redux/api/kb.js
+++ src/redux/api/kb.js
@@ -5,14 +5,14 @@ import axios from 'axios';
 //const urlTiket = 'localhost:3001/tickets';
 export function kb() {
     return dispatch => {
-        axios.get(`https://api.ict-servicedesk.xyz/knowledge_base`, {
+        axios.get(`https://api.ict-servicedesk.xyz/knowledge_base_technician`, {
             headers: {
                 key: "8dfcb234a322aeeb6b530f20c8e9988e"
             }
         })
             .then(res => {
                 const kb = res.data.values;
-                console.log("data", kb)
+                // console.log("data", kb)
                 dispatch(fetchKb(kb));
             })
     }
