2a52060d15663f9b16c6a93415693a279cad56fd
MutiaraFT <mutiarafitri99@mail.ugm.ac.id>
2020-07-26 02:40:44 +0700

    update edit

--- src/components/views/navbar/NavbarProfile.js
+++ src/components/views/navbar/NavbarProfile.js
@@ -5,22 +5,55 @@ import { connect } from 'react-redux';
 import users from '../../../redux/api/users';
 import { Link } from 'react-router-dom';
 import { prod } from '../../../redux/url/server';
+import axios from 'axios';
+const jwt = require('jsonwebtoken');
 
 
 export class NavbarProfile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
 
+        }
+    }
     componentDidMount() {
         this.props.userku();
+
+        jwt.verify(localStorage.getItem("jwt"), 'dimasputray', (err, decoded) => {
+            if (err) {
+                console.log("Error", err)
+                localStorage.removeItem("jwt");
+                // dispatch(loginFailed("Your session has expired"));
+            }
+            const user = decoded.data;
+            this.setState({ ...user },
+                () => {
+                    this.fetchUser();
+                }
+            )
+
+        });
     }
+    fetchUser = () => {
+        axios.get(`https://api.ict-servicedesk.xyz/user/technician/` + this.state.user_id, {
+            headers: {
+                key: "8dfcb234a322aeeb6b530f20c8e9988e"
+            }
+        })
+            .then(res => {
+                const user = res.data.values[0];
+                this.setState({ ...user })
+            })
+    }
+
     render() {
         const { data } = this.props;
 
 
         // const back = (this.props.back == "true") ?<Link to="." ><div className="back-button"><img src={arrow} alt="" style={{padding:"15px"}}/></div></Link>:'';
         // const title = (this.props.title) ? <div className="title-pages" style={{paddingTop:"5px"}}>{this.props.title}</div>:<img src={logo} alt="Komatsu" style={{ margin: "0 auto", height: "30px",paddingTop:"15px" }} />;
-        const profile = (data.personState.data)  ? data.personState.data.user_image ? data.personState.data.user_image : "defaultEmploy.png":"defaultEmploy.png"
-        const gambar = prod + 'avatar/technician/' + profile
-        console.log(gambar)
+        const profile = this.state.technician_image ? "https://api.ict-servicedesk.xyz/avatar/technician/" + this.state.technician_image : "https://api.ict-servicedesk.xyz/avatar/technician/defaultEmploy.png";
+        console.log(profile)
         return (
             <div className="container" style={{ width: "100%" }}>
                 <Link to='/'>
@@ -32,8 +65,8 @@ export class NavbarProfile extends Component {
                     <img src={profileBg} alt="profile" style={{ marginTop: "-90px", width: "100%" }} />
                 </div>
                 <div style={{ position: "absolute", top: "175px", width: "100%", left: "0px" }}>
-                    <div style={{ color: "black", fontSize: "20px" }}> {(data.personState.data) ? (data.personState.data.data) ? data.personState.data.data.user_firstname : "" : ""} {(data.personState.data) ? (data.personState.data.data) ? data.personState.data.data.user_lastname : "" : ""} </div>
-                    <div style={{ color: "black", fontSize: "14px" }}>Technician ID <span style={{ fontSize: "14px" }}> {(data.personState.data) ? (data.personState.data.data) ? data.personState.data.data.user_id : "" : ""}</span></div>
+                    <div style={{ color: "black", fontSize: "20px" }}> {(this.state.technician_firstname) ? this.state.technician_firstname : ""} {(this.state.technician_lastname) ? this.state.technician_lastname : ""} </div>
+                    <div style={{ color: "black", fontSize: "14px" }}>Technician ID : <span style={{ fontSize: "14px", color: "black" }}> {(this.state.user_id) ? this.state.user_id : ""}</span></div>
                 </div>
                 <div className="profile"
                     style={{
@@ -48,7 +81,7 @@ export class NavbarProfile extends Component {
                         position: "absolute",
                         overflow: "hidden"
                     }}>
-                    <img src={gambar} alt="img" style={{ width: "100%" }} />
+                    <img src={profile} alt="img" style={{ width: "100%" }} />
                 </div>
             </div>
         )
